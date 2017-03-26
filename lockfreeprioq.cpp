cc947d9e73a3f770c7e587a1b9c9af08d2e4b5e8
DBrickeen <dcbrickeen@knights.ucf.edu>
2017-03-26 04:27:42 -0400

    Update after debugging
    It compiles now, but does not link due to the fact that atomic<CMNode>* apparently cannot be done in C++11, due to the struct being too large (I was under the impression that C++ would implement atomics on large data with mutexes, but apparently not).

--- lockfreeprioq.cpp
+++ lockfreeprioq.cpp
@@ -61,26 +61,26 @@ public:
         srand(time(0));
     }
 
-    T value(CMNode n) {
-        return n->list ? n->list->value : maxValue;
+    T getvalue(CMNode n) {
+        return n.list ? n.list->value : maxValue;
     }
 
     void insert(T value) {
         while (true) {
             int n = findInsertPoint(value);
             int x = indexX(n), y = indexY(n, x);
-            CMNode* C = DCASRead(tree[x] + y);
-            if (value(*C) >= value) {
-                CMNode CP = CMNode { new LNode { value, C->list }, C->dirty, C->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0 };
+            CMNode C = DCASRead(tree[x] + y);
+            if (getvalue(C) >= value) {
+                CMNode CP = CMNode { new LNode { value, C.list }, C.dirty, C.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0 };
                 if (n == 1) {
-                    if (tree[x][y].compare_exchange_weak(C, CP))
+                    if (atomic_compare_exchange_weak(tree[x] + y, &C, CP))
                         return;
                     else {
                         int p = (n - 1) / 2;
                         int px = indexX(p), py = indexY(p, px);
-                        CMNode* P = DCASRead(tree[px] + py);
-                        if (value(*P) <= value) {
-                            CMNode* desc = buildDCSS1(tree[x] + y, C, &CP, tree[px] + py, P);
+                        CMNode P = DCASRead(tree[px] + py);
+                        if (getvalue(P) <= value) {
+                            atomic<CMNode>* desc = buildDCSS1(tree[x] + y, &C, &CP, tree[px] + py, &P);
                             if (DCSS(desc)) {
                                 delete(desc);
                                 return;
@@ -96,10 +96,12 @@ public:
 
     int findInsertPoint(T v) {
         while (true) {
-            int d = READ(depth);
+            int d = depth;
             for (int i = 0; i < THRESHOLD; ++i) {
                 int n = randLeaf(d);
-                if (value(n) >= v)
+                int x = indexX(n), y = indexY(n, x);
+                CMNode N = DCASRead(tree[x] + y);
+                if (getvalue(N) >= v)
                     return bs(n, v);
             }
             atomic_compare_exchange_weak(&depth, &d, d + 1);
@@ -112,28 +114,28 @@ public:
             return n;
         int p = (n - 1) / 2;
         int px = indexX(p), py = indexY(p, px);
-        if (value(tree[px][py]) < v)
+        if (getvalue(tree[px][py]) < v)
             return n;
         return bs(p, v);
     }
 
-    int randLeaf() {
-        return (int) rand() % (int) pow(2, depth - 1)
-            + (int) pow(2, depth - 1) - 1;
+    int randLeaf(int d) {
+        return (int) rand() % (int) pow(2, d - 1)
+            + (int) pow(2, d - 1) - 1;
     }
 
     T extractMin() {
         while (true) {
-            CMNode* R = DCASRead(tree[0] + 0);
-            if (R->dirty) {
+            CMNode R = DCASRead(tree[0] + 0);
+            if (R.dirty) {
                 moundify(1);
                 continue;
             }
-            if (R->list == NULL)
+            if (R.list == NULL)
                 return maxValue;
-            if (tree[0][0].compare_exchange_weak(R, CMNode {R->list->next, true, R->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0})) {
-                T retval = R->list->value;
-                delete(R->list);
+            if (atomic_compare_exchange_weak(tree[0] + 0, &R, CMNode {R.list->next, true, R.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0})) {
+                T retval = R.list->value;
+                delete(R.list);
                 moundify(1);
                 return retval;
             }
@@ -143,49 +145,49 @@ public:
     void moundify(int n) {
         while (true) {
             int x = indexX(n), y = indexY(n, x);
-            CMNode* N = DCASRead(tree[x] + y);
-            int d = READ(depth);
-            if (!N->dirty)
+            CMNode N = DCASRead(tree[x] + y);
+            int d = depth;
+            if (!N.dirty)
                 return;
             if (n >= (1<<(d-1)) && n <= (1<<d) - 1)
                 return;
             int lx = indexX(2 * n), ly = indexY(2 * n, lx);
-            CMNode* L = DCASRead(tree[lx] + ly);
+            CMNode L = DCASRead(tree[lx] + ly);
             int rx = indexX(2 * n + 1), ry = indexY(2 * n + 1, rx);
-            CMNode* R = DCASRead(tree[rx] + ry);
-            if (L->dirty) {
+            CMNode R = DCASRead(tree[rx] + ry);
+            if (L.dirty) {
                 moundify(2 * n);
                 continue;
             }
-            if (R->dirty) {
+            if (R.dirty) {
                 moundify(2 * n + 1);
                 continue;
             }
-            if (value(*L) <= value(*R) && value(*L) < value(*N)) {
-                CMNode* desc = buildDCAS(tree[x] + y, N, new CMNode {L->list, false, N->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0 },
-                                   tree[lx] + ly, L, new CMNode {N->list, true, L->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0 });
+            if (getvalue(L) <= getvalue(R) && getvalue(L) < getvalue(N)) {
+                atomic<CMNode>* desc = buildDCAS(tree[x] + y, &N, new CMNode {L.list, false, N.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0 },
+                                   tree[lx] + ly, &L, new CMNode {N.list, true, L.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0 });
                 if (DCAS(desc)) {
-                    delete(desc->status);
-                    delete(desc->n1);
-                    delete(desc->n2);
+                    delete(desc->load().status);
+                    delete(desc->load().n1);
+                    delete(desc->load().n2);
                     delete(desc);
                     moundify(2 * n);
                     return;
                 }
             }
-            else if (value(*R) < value(*L) && value(*R) < value(*N)) {
-                CMNode* desc = buildDCAS(tree[x] + y, N, new CMNode {R->list, false, N->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0},
-                                   tree[rx] + ry, R, new CMNode {N->list, true, R->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0});
+            else if (getvalue(R) < getvalue(L) && getvalue(R) < getvalue(N)) {
+                atomic<CMNode>* desc = buildDCAS(tree[x] + y, &N, new CMNode {R.list, false, N.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0},
+                                   tree[rx] + ry, &R, new CMNode {N.list, true, R.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0});
                 if (DCAS(desc)) {
-                    delete(desc->status);
-                    delete(desc->n1);
-                    delete(desc->n2);
+                    delete(desc->load().status);
+                    delete(desc->load().n1);
+                    delete(desc->load().n2);
                     delete(desc);
                     moundify(2 * n + 1);
                     return;
                 }
             }
-            else if (tree[x][y].compare_exchange_weak(N, CMNode {N->list, false, N->c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0}))
+            else if (atomic_compare_exchange_weak(tree[x] + y, &N, CMNode {N.list, false, N.c + 1, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0}))
                 return;
         }
     }
@@ -209,95 +211,102 @@ public:
     // "A Practical Multi-word Compare-and-Swap Operation"
     // by Harris, Fraser, and Pratt
     CMNode* CAS(atomic<CMNode>* tree, CMNode *C, CMNode CP) {
-        if (tree->compare_exchange_weak(C, CP))
+        if (atomic_compare_exchange_weak(tree, C, CP))
             return C;
         return C;
     }
 
-    CMNode* DCSS(CMNode *d) {
-        CMNode *r = NULL;
+    CMNode* DCSS(atomic<CMNode>* d) {
+        CMNode* r = NULL;
         do {
-            r = CAS(d->a2, d->o2, *d);
-            if (r && r->isDCSS)
-                Complete(r);
-        } while (r && r->isDCSS);
-        if (r == *(d->o2))
+            r = CAS(d->load().a2, d->load().o2, *d);
+            if (r->isDCSS) {
+                atomic<CMNode>* temp = new atomic<CMNode> { *r };
+                Complete(temp);
+                delete(temp);
+            }
+        } while (r->isDCSS);
+        if (*r == *(d->load().o2))
             Complete(d);
         return r;
     }
 
-    CMNode* DCSSRead(CMNode* d) {
+    CMNode* DCSSRead(atomic<CMNode>* d) {
         CMNode* r = NULL;
+        CMNode dl = d->load();
         do {
-            r = CAS(d->a2, d->o2, *d);
-            if (r && r->isDCSS) Complete(r);
-        } while (r && r->isDCSS);
+            r = CAS(dl.a2, dl.o2, *d);
+            if (r->isDCSS) {
+                atomic<CMNode>* temp = new atomic<CMNode> { *r };
+                Complete(temp);
+                delete(temp);
+            }
+        } while (r->isDCSS);
         return r;
     }
 
-    void Complete(CMNode *d) {
-        CMNode v = READ(*(d->a1));
-        if (v == *(d->o1))
-            CAS(d->a2, d, *(d->n2));
+    void Complete(atomic<CMNode>* d) {
+        CMNode dl = d->load();
+        CMNode v = dl.a1->load();
+        if (v == *(dl.o1))
+            CAS(dl.a2, &dl, *(dl.n2));
         else
-            CAS(d->a2, d, *(d->o2));
+            CAS(dl.a2, &dl, *(dl.o2));
     }
 
-    bool DCAS(CMNode* cd) {
+    bool DCAS(atomic<CMNode>* cd) {
         int status = 0;
-        if (READ(*(cd->status)).check == 0) {
+        CMNode cdl = cd->load();
+        if (cdl.status->load().check == 0) {
             status = 2;
             for (int i = 0; i < 2 && status == 2; ++i) {
-                CMNode* val = DCSS(buildDCSS2(cd->status, buildStat(0), i ? cd->a2 : cd->a1, i ? cd->o2 : cd->o1, cd));
+                CMNode* val = DCSS(buildDCSS2(cdl.status, buildStat(0), i ? cdl.a2 : cdl.a1, i ? cdl.o2 : cdl.o1, &cdl));
                 if (val->isDCAS) {
-                    if (val != cd) {
-                        DCAS(val);
+                    if (val == &cdl) {
+                        atomic<CMNode>* temp = new atomic<CMNode> { *val };
+                        DCAS(temp);
+                        delete(temp);
                         i--;
                     }
                 }
-                else if (val != i ? cd->o2 : cd->o1)
+                else if (val != (i ? cdl.o2 : cdl.o1))
                     status = 1;
             }
-            CAS(cd->status, buildStat(0), buildStat(status));
+            CAS(cdl.status, buildStat(0), *buildStat(status));
         }
-        bool succeeded = (READ(*(cd->status)).check == 2);
+        bool succeeded = (cdl.status->load().check == 2);
         for (int i = 0; i < 2; ++i)
-            CAS(i ? cd->a2 : cd->a1, cd, succeeded ? i ? *(cd->n2) : *(cd->n1) : i ? *(cd->o2) : *(cd->o1));
+            CAS(i ? cdl.a2 : cdl.a1, &cdl, succeeded ? i ? *(cdl.n2) : *(cdl.n1) : i ? *(cdl.o2) : *(cdl.o1));
         return succeeded;
     }
 
-    CMNode* DCASRead(CMNode* addr) {
+    CMNode DCASRead(atomic<CMNode>* addr) {
         CMNode* r = NULL;
         do {
             r = DCSSRead(addr);
-            if (r && r->isDCAS) DCAS(r);
-        } while (r && r->isDCAS);
-        return r;
+            if (r->isDCAS) {
+                atomic<CMNode>* temp = new atomic<CMNode> { *r };
+                DCAS(temp);
+                delete(temp);
+            }
+        } while (r->isDCAS);
+        return *r;
     }
 
-    CMNode* buildDCSS1(atomic<CMNode>* a2, CMNode* o2, CMNode* n2, atomic<CMNode>* a1, CMNode* o1) {
-        return new CMNode { NULL, false, 0, true, false, a1, o1, NULL, a2, o2, n2, 0, NULL, 0 };
+    atomic<CMNode>* buildDCSS1(atomic<CMNode>* a2, CMNode* o2, CMNode* n2, atomic<CMNode>* a1, CMNode* o1) {
+        return new atomic<CMNode> { CMNode { NULL, false, 0, true, false, a1, o1, NULL, a2, o2, n2, 0, NULL, 0 } };
     }
 
-    CMNode* buildDCSS2(atomic<CMNode>* a1, CMNode* o1, atomic<CMNode>* a2, CMNode *o2, CMNode* n2) {
-        return new CMNode { NULL, false, 0, true, false, a1, o1, NULL, a2, o2, n2, 0, NULL, 0 };
+    atomic<CMNode>* buildDCSS2(atomic<CMNode>* a1, CMNode* o1, atomic<CMNode>* a2, CMNode *o2, CMNode* n2) {
+        return new atomic<CMNode> { CMNode { NULL, false, 0, true, false, a1, o1, NULL, a2, o2, n2, 0, NULL, 0 } };
     }
 
-    CMNode* buildDCAS(atomic<CMNode>* a1, CMNode* o1, CMNode* n1, atomic<CMNode>* a2, CMNode *o2, CMNode *n2) {
-        atomic<CMNode>* temp = new CMNode { NULL, false, 0, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0};
-        return new CMNode { NULL, false, 0, false, true, a1, o1, n1, a2, o2, n2, 0, temp, 0 };
+    atomic<CMNode>* buildDCAS(atomic<CMNode>* a1, CMNode* o1, CMNode* n1, atomic<CMNode>* a2, CMNode *o2, CMNode *n2) {
+        atomic<CMNode>* temp = new atomic<CMNode> { CMNode { NULL, false, 0, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0} };
+        return new atomic<CMNode> { CMNode { NULL, false, 0, false, true, a1, o1, n1, a2, o2, n2, 0, temp, 0 } };
     }
 
     CMNode* buildStat(int n) {
         return new CMNode { NULL, false, 0, false, false, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, n };
     }
-
-    CMNode READ(atomic<CMNode> n) {
-        return n;
-    }
-
-    int READ(atomic<int> n) {
-        return n;
-    }
-
 };
